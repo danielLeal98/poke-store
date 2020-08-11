56a8b20595d55974532e8dc944b2087e31c6e1b1
Daniel Matheus <danielmatheus2015@outlook.com>
2020-08-11 12:37:44 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+
+import AppProvider from './hooks/index';
+
+import Grass from './pages/Grass';
+import Psychic from './pages/Psychic';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <AppProvider>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/grass" exact component={Grass} />
+          <Route path="/psychic" exact component={Psychic} />
+        </Switch>
+      </Router>
+      <GlobalStyle />
+    </AppProvider>
+  );
+}
+
+export default App;
